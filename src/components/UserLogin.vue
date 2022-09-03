9ff5fd2d33dd83aa2748de3a6987cdeedc47a6ea
jeric realubit <jericrealubit@gmail.com>
2022-09-04 09:57:45 +1200

    doing login, debug $ref

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -12,20 +12,20 @@
                 <v-card class="px-4">
                 <v-card-text>
                     <v-form ref="loginForm" v-model="valid" lazy-validation>
-                    <v-row>
-                        <v-col cols="12">
-                        <v-text-field v-model="loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                        <v-text-field v-model="loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                        </v-col>
-                        <v-col class="d-flex" cols="12" sm="6" xsm="12">
-                        </v-col>
-                        <v-spacer></v-spacer>
-                        <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
-                        <v-btn x-large block :disabled="!valid" color="success" @click="validate"> Login </v-btn>
-                        </v-col>
-                    </v-row>
+                        <v-row>
+                            <v-col cols="12">
+                            <v-text-field v-model="loginFormValue.loginEmail" :rules="loginEmailRules" label="E-mail" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                            <v-text-field v-model="loginFormValue.loginPassword" :append-icon="show1?'eye':'eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                            </v-col>
+                            <v-col class="d-flex" cols="12" sm="6" xsm="12">
+                            </v-col>
+                            <v-spacer></v-spacer>
+                            <v-col class="d-flex" cols="12" sm="3" xsm="12" align-end>
+                            <v-btn x-large block :disabled="!valid" color="success" @click="validate"> Login </v-btn>
+                            </v-col>
+                        </v-row>
                     </v-form>
                 </v-card-text>
                 </v-card>
@@ -34,27 +34,27 @@
                 <v-card class="px-4">
                 <v-card-text>
                     <v-form ref="registerForm" v-model="valid" lazy-validation>
-                    <v-row>
-                        <v-col cols="12" sm="6" md="6">
-                        <v-text-field v-model="formValues.firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12" sm="6" md="6">
-                        <v-text-field v-model="formValues.lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                        <v-text-field v-model="formValues.email" :rules="emailRules" label="E-mail" required></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                        <v-text-field v-model="formValues.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
-                        </v-col>
-                        <v-col cols="12">
-                        <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
-                        </v-col>
-                        <v-spacer></v-spacer>
-                        <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
-                        <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
-                        </v-col>
-                    </v-row>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="formValues.firstName" :rules="[rules.required]" label="First Name" maxlength="20" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="6">
+                            <v-text-field v-model="formValues.lastName" :rules="[rules.required]" label="Last Name" maxlength="20" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                            <v-text-field v-model="formValues.email" :rules="emailRules" label="E-mail" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                            <v-text-field v-model="formValues.password" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, rules.min]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Password" hint="At least 8 characters" counter @click:append="show1 = !show1"></v-text-field>
+                            </v-col>
+                            <v-col cols="12">
+                            <v-text-field block v-model="verify" :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'" :rules="[rules.required, passwordMatch]" :type="show1 ? 'text' : 'password'" name="input-10-1" label="Confirm Password" counter @click:append="show1 = !show1"></v-text-field>
+                            </v-col>
+                            <v-spacer></v-spacer>
+                            <v-col class="d-flex ml-auto" cols="12" sm="3" xsm="12">
+                            <v-btn x-large block :disabled="!valid" color="success" @click="validate">Register</v-btn>
+                            </v-col>
+                        </v-row>
                     </v-form>
                 </v-card-text>
                 </v-card>
@@ -86,7 +86,11 @@
             lastName: "",
             email: "",
             password: "",
-        },      
+        },    
+        loginFormValue: {
+            loginPassword: "",
+            loginEmail: "",
+        },
         verify: "",
         loginPassword: "",
         loginEmail: "",
@@ -112,8 +116,7 @@
         },
         methods: {
             register(formData) {
-                console.log(formData)
-
+                // console.log(formData)
                 fetch(apiUsers, {
                     method: "POST",
                     headers: { 'Content-Type': 'application/json' },
@@ -133,6 +136,7 @@
                     this.register(this.formValues);
                 }
                 if (this.$refs.loginForm.validate()) {
+                    console.log(this.loginFormValue)
                     // submit form to server/API here...
                 }
             },
