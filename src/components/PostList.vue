df5f2e9531b0e231244664cddd213aacc7948273
jeric realubit <jericrealubit@gmail.com>
2022-09-05 16:27:39 +1200

    integrating image uploader to posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -35,8 +35,26 @@
       </template>
 
       <template v-slot:[`item.productimage`]="{ item }">
-        <v-text-field v-model="editedItem.productimage" :hide-details="true" dense single-line :autofocus="true" v-if="item._id === editedItem._id"></v-text-field>
-        <span v-else>{{item.productimage}}</span>
+        <!-- <v-text-field v-model="editedItem.productimage" :hide-details="true" dense single-line :autofocus="true" v-if="item._id === editedItem._id"></v-text-field> -->
+        <v-file-input v-if="item._id === editedItem._id"
+          v-model="editedItem.productimage"
+          :hide-details="true"
+          dense
+          single-line
+          :autofocus="true"
+          accept="image/*"
+          show-size
+          prepend-icon="mdi-camera"
+          @change="uploadImage"
+        ></v-file-input>
+        <span v-else>
+          <v-img
+            lazy-src="https://picsum.photos/id/11/10/6"
+            max-height="50"
+            max-width="50"
+            :src="item.productimage"
+          ></v-img>
+        </span>
       </template>
 
       <template v-slot:[`item.price`]="{ item }">
@@ -87,6 +105,8 @@
 </template>
 
 <script>
+  const axios = require("axios");
+  const formData = require("form-data");
   const api = "https://api-posts-jeric.netlify.app/.netlify/functions/api/"
   export default {
     name: 'PostsList',
@@ -106,7 +126,7 @@
         multipleImageUrl: ''
       },
       headTitle: [
-        { text: "Image", value: "productimage" },
+        { text: "Item", value: "productimage" },
         { text: "Price", value: "price" },
         { text: "Title", value: "title" },
         { text: "Description", value: "description" },
@@ -216,6 +236,39 @@
           })
         }
         this.close()
+      },
+      uploadImage(event) {
+        try {
+        console.log("ping !");
+        const bodyFormData = new formData();
+        bodyFormData.append("image", event); // event = is our image object
+
+        const apiUrl = "https://api.imgbb.com/1/upload";
+        const apiKey = "bdcadd576cf26e4fe1f31de4594ca4fd";
+        axios({
+          method: "POST",
+          url: apiUrl + "?key=" + apiKey,
+          data: bodyFormData,
+          header: { "Content-Type": "multipart/form-data" }
+          })
+          .then((response) => {
+            console.log("API response ↓");
+            console.log(response);
+            console.log(response.data.data.url) // image url
+            this.uploadedImage = response.data.data.url; // assign to data property
+          })
+          .catch((err) => {
+          console.log("API error ↓");
+          console.log(err);
+
+          if (err.response.data.error) {
+            console.log(err.response.data.error);
+            //When trouble shooting, simple informations about the error can be found in err.response.data.error so it's good to display it
+          }
+          });
+        } catch (error) {
+          console.log(error);
+        }
       },
     },
     mounted() {
