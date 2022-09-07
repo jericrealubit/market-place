a07db9bb6a119e666e3fbe479e6b4c1b7bc9ddd3
jeric realubit <jericrealubit@gmail.com>
2022-09-07 22:55:15 +1200

    message elements

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,17 +1,20 @@
 <template>
   <v-container>
     <!-- details -->
-    <v-dialog v-model="detailsDialog">
-      <v-card class="mx-auto" max-width="400">
+    <v-dialog v-model="detailsDialog" max-width="40%" max-height="50%">
+      <v-card>
+        <v-card-title>Message SellerName</v-card-title>
+        <v-icon large>mdi-close-circle-outline</v-icon>
+        <v-divider></v-divider>
         <v-img
-          class="white--text align-end"
-          height="200px"
+          class="white--text rounded-lg"
+          width="65px"
+          height="65px"
           :src="details.productimage"
         >
-          <v-card-title
-            >$ {{ details.price }} - {{ details.title }}</v-card-title
-          >
         </v-img>
+
+        <v-card-title>$ {{ details.price }} - {{ details.title }}</v-card-title>
 
         <v-card-subtitle class="pb-0">{{ details.location }}</v-card-subtitle>
 
@@ -19,11 +22,34 @@
           <div>{{ details.description }}</div>
         </v-card-text>
 
+          <v-btn rounded>I'm interested in this item.</v-btn>
+          <v-btn rounded>Is this item still available?</v-btn>
+          <v-btn rounded>What condition is this item in?</v-btn>
+          <v-btn rounded>Do you deliver?</v-btn>
+          
+        <v-textarea
+          outlined
+          name="input-7-4"
+          label="Please type your message to the seller"
+          class="px-4"
+        ></v-textarea>
+        
+        <div class="text-no-wrap">Don't share your email, phone or financial information.</div>
+        <v-divider></v-divider>
         <v-card-actions>
-          <v-btn color="orange" text> Share </v-btn>
-
-          <v-btn color="orange" text> Explore </v-btn>
+          <v-btn text @click="(agreement = false), (dialog = false)">
+            Cancel
+          </v-btn>
+          <v-spacer></v-spacer>
+          <v-btn
+            class="white--text"
+            color="deep-purple accent-4"
+            @click="(agreement = true), (dialog = false)"
+          >
+            Send Message
+          </v-btn>
         </v-card-actions>
+
       </v-card>
     </v-dialog>
 
@@ -230,250 +256,250 @@
 </template>
 
 <script>
-  const axios = require("axios");
-  const formData = require("form-data");
-  const api = "https://api-posts-jeric.netlify.app/.netlify/functions/api/";
-  export default {
-    name: "PostsList",
+const axios = require("axios");
+const formData = require("form-data");
+const api = "https://api-posts-jeric.netlify.app/.netlify/functions/api/";
+export default {
+  name: "PostsList",
 
-    data: () => ({
-      details: {
-        productimage: "",
-        price: "",
-        title: "",
-        description: "",
-        location: "",
-        user_id: "",
-      },
-      detailsDialog: false,
-      postsLoading: true,
-      msg: "",
-      add: true,
-      search: "",
-      loading: true,
-      showPassword: false,
-      posts: [],
-      id: "", // id to update PUT:id, was set by GET:id
-      headTitle: [
-        { text: "Item", value: "productimage" },
-        { text: "Price", value: "price" },
-        { text: "Title", value: "title" },
-        { text: "Description", value: "description" },
-        { text: "Location", value: "location" },
-        { text: "Actions", value: "actions" },
-      ],
-      formValues: {
-        productimage: "",
-        price: "",
-        title: "",
-        description: "",
-        location: "",
-        user_id: "",
-      },
-      editedIndex: -1,
-      editedItem: {
-        productimage: "",
-        price: "",
-        title: "",
-        description: "",
-        location: "",
-        user_id: "",
-      },
-      defaultItem: {
-        productimage: "",
-        price: "",
-        title: "",
-        description: "",
-        location: "",
-        user_id: "",
-      },
-    }),
-    methods: {
-      showImage(imageUrls) {
-        let urls = "";
-        if (imageUrls) {
-          const imgArr = imageUrls.split(",");
-          imgArr.forEach((element) => {
-            urls += `<img src="${element}" width="50">`;
-          });
-        }
-        return urls;
-      },
-      getAll() {
-        fetch(api)
-          .then((response) => response.json())
-          .then((data) => {
-            // filter data to show only post that match the user_id
-            if (localStorage.userId) {
-              let postData = [];
-              data.forEach((element) => {
-                if (localStorage.userId == element.user_id) {
-                  postData.push(element);
-                }
-              });
-              this.posts = postData;
-            } else {
-              this.posts = data;
-            }
-            this.postsLoading = false;
-            this.loading = false;
-          })
-          .catch((err) => {
-            if (err) throw err;
-          });
-      },
-      editItem(item) {
-        this.editedIndex = this.posts.indexOf(item);
-        this.editedItem = Object.assign({}, item);
-        this.id = item._id;
-        this.add = false;
-      },
-      deleteItem(item) {
-        if (confirm("Are you sure you want to delete this item?")) {
-          const index = this.posts.indexOf(item);
-          this.posts.splice(index, 1);
-          fetch(api + item._id, {
-            method: "DELETE",
-          })
-            .then((response) => response.text())
-            .then((data) => {
-              this.msg = data;
-              this.getAll();
-            })
-            .catch((err) => {
-              if (err) throw err;
-            });
-          console.log("confirm delete");
-        } else {
-          console.log("cancel delete");
-        }
-      },
-      close() {
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem);
-          this.editedIndex = -1;
-          if (this.add) {
-            this.posts.shift(this.editedItem);
-            this.editItem(this.editedItem);
-          }
-        }, 300);
-      },
-      addNew() {
-        const addObj = Object.assign({}, this.defaultItem);
-        this.posts.unshift(addObj);
-        this.editItem(addObj);
-        this.add = true;
-      },
-      save() {
-        if (this.editedIndex > -1) {
-          this.formValues.price = this.editedItem.price;
-          this.formValues.title = this.editedItem.title;
-          this.formValues.description = this.editedItem.description;
-          this.formValues.location = this.editedItem.location;
-          Object.assign(this.posts[this.editedIndex], this.editedItem);
-          let fetchApi = this.id ? api + this.id : api;
-          let fetchMethod = this.id ? "PUT" : "POST";
-          fetch(fetchApi, {
-            method: fetchMethod,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(this.formValues),
-          })
-            .then((response) => response.text())
-            .then((data) => {
-              this.msg = data;
-              this.getAll();
-            })
-            .catch((err) => {
-              if (err) throw err;
-            });
-        }
-        this.close();
-      },
-      uploadImage(event) {
-        try {
-          console.log("ping !");
-          const bodyFormData = new formData();
-          bodyFormData.append("image", event); // event = is our image object
-
-          const apiUrl = "https://api.imgbb.com/1/upload";
-          const apiKey = "bdcadd576cf26e4fe1f31de4594ca4fd";
-          axios({
-            method: "POST",
-            url: apiUrl + "?key=" + apiKey,
-            data: bodyFormData,
-            header: { "Content-Type": "multipart/form-data" },
-          })
-            .then((response) => {
-              console.log("API response ↓");
-              console.log(response);
-              console.log(response.data.data.url); // image url
-              this.uploadedImage = response.data.data.url; // assign to data property
-              this.formValues.productimage = response.data.data.url;
-            })
-            .catch((err) => {
-              console.log("API error ↓");
-              console.log(err);
-
-              if (err.response.data.error) {
-                console.log(err.response.data.error);
-                //When trouble shooting, simple informations about the error can be found in err.response.data.error so it's good to display it
+  data: () => ({
+    details: {
+      productimage: "",
+      price: "",
+      title: "",
+      description: "",
+      location: "",
+      user_id: "",
+    },
+    detailsDialog: false,
+    postsLoading: true,
+    msg: "",
+    add: true,
+    search: "",
+    loading: true,
+    showPassword: false,
+    posts: [],
+    id: "", // id to update PUT:id, was set by GET:id
+    headTitle: [
+      { text: "Item", value: "productimage" },
+      { text: "Price", value: "price" },
+      { text: "Title", value: "title" },
+      { text: "Description", value: "description" },
+      { text: "Location", value: "location" },
+      { text: "Actions", value: "actions" },
+    ],
+    formValues: {
+      productimage: "",
+      price: "",
+      title: "",
+      description: "",
+      location: "",
+      user_id: "",
+    },
+    editedIndex: -1,
+    editedItem: {
+      productimage: "",
+      price: "",
+      title: "",
+      description: "",
+      location: "",
+      user_id: "",
+    },
+    defaultItem: {
+      productimage: "",
+      price: "",
+      title: "",
+      description: "",
+      location: "",
+      user_id: "",
+    },
+  }),
+  methods: {
+    showImage(imageUrls) {
+      let urls = "";
+      if (imageUrls) {
+        const imgArr = imageUrls.split(",");
+        imgArr.forEach((element) => {
+          urls += `<img src="${element}" width="50">`;
+        });
+      }
+      return urls;
+    },
+    getAll() {
+      fetch(api)
+        .then((response) => response.json())
+        .then((data) => {
+          // filter data to show only post that match the user_id
+          if (localStorage.userId) {
+            let postData = [];
+            data.forEach((element) => {
+              if (localStorage.userId == element.user_id) {
+                postData.push(element);
               }
             });
-        } catch (error) {
-          console.log(error);
-        }
-      },
-      showDetails(post_id) {
-        console.log(post_id);
-        //
-
-        fetch(api + post_id, {
-          method: "GET",
+            this.posts = postData;
+          } else {
+            this.posts = data;
+          }
+          this.postsLoading = false;
+          this.loading = false;
         })
-          .then((response) => response.json())
+        .catch((err) => {
+          if (err) throw err;
+        });
+    },
+    editItem(item) {
+      this.editedIndex = this.posts.indexOf(item);
+      this.editedItem = Object.assign({}, item);
+      this.id = item._id;
+      this.add = false;
+    },
+    deleteItem(item) {
+      if (confirm("Are you sure you want to delete this item?")) {
+        const index = this.posts.indexOf(item);
+        this.posts.splice(index, 1);
+        fetch(api + item._id, {
+          method: "DELETE",
+        })
+          .then((response) => response.text())
           .then((data) => {
-            console.log(data);
-            this.details.productimage = data.productimage;
-            this.details.price = data.price;
-            this.details.title = data.title;
-            this.details.description = data.description;
-            this.details.location = data.location;
-            this.details.user_id = data.user_id;
-            console.log(this.details);
+            this.msg = data;
+            this.getAll();
           })
           .catch((err) => {
             if (err) throw err;
           });
         console.log("confirm delete");
-
-        this.detailsDialog = true;
-      },
-    },
-    mounted() {
-      this.getAll();
-      if (localStorage.userId) {
-        // set user_id
-        this.formValues.user_id = localStorage.userId;
+      } else {
+        console.log("cancel delete");
       }
     },
-  };
+    close() {
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem);
+        this.editedIndex = -1;
+        if (this.add) {
+          this.posts.shift(this.editedItem);
+          this.editItem(this.editedItem);
+        }
+      }, 300);
+    },
+    addNew() {
+      const addObj = Object.assign({}, this.defaultItem);
+      this.posts.unshift(addObj);
+      this.editItem(addObj);
+      this.add = true;
+    },
+    save() {
+      if (this.editedIndex > -1) {
+        this.formValues.price = this.editedItem.price;
+        this.formValues.title = this.editedItem.title;
+        this.formValues.description = this.editedItem.description;
+        this.formValues.location = this.editedItem.location;
+        Object.assign(this.posts[this.editedIndex], this.editedItem);
+        let fetchApi = this.id ? api + this.id : api;
+        let fetchMethod = this.id ? "PUT" : "POST";
+        fetch(fetchApi, {
+          method: fetchMethod,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(this.formValues),
+        })
+          .then((response) => response.text())
+          .then((data) => {
+            this.msg = data;
+            this.getAll();
+          })
+          .catch((err) => {
+            if (err) throw err;
+          });
+      }
+      this.close();
+    },
+    uploadImage(event) {
+      try {
+        console.log("ping !");
+        const bodyFormData = new formData();
+        bodyFormData.append("image", event); // event = is our image object
+
+        const apiUrl = "https://api.imgbb.com/1/upload";
+        const apiKey = "bdcadd576cf26e4fe1f31de4594ca4fd";
+        axios({
+          method: "POST",
+          url: apiUrl + "?key=" + apiKey,
+          data: bodyFormData,
+          header: { "Content-Type": "multipart/form-data" },
+        })
+          .then((response) => {
+            console.log("API response ↓");
+            console.log(response);
+            console.log(response.data.data.url); // image url
+            this.uploadedImage = response.data.data.url; // assign to data property
+            this.formValues.productimage = response.data.data.url;
+          })
+          .catch((err) => {
+            console.log("API error ↓");
+            console.log(err);
+
+            if (err.response.data.error) {
+              console.log(err.response.data.error);
+              //When trouble shooting, simple informations about the error can be found in err.response.data.error so it's good to display it
+            }
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    showDetails(post_id) {
+      console.log(post_id);
+      //
+
+      fetch(api + post_id, {
+        method: "GET",
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+          this.details.productimage = data.productimage;
+          this.details.price = data.price;
+          this.details.title = data.title;
+          this.details.description = data.description;
+          this.details.location = data.location;
+          this.details.user_id = data.user_id;
+          console.log(this.details);
+        })
+        .catch((err) => {
+          if (err) throw err;
+        });
+      console.log("confirm delete");
+
+      this.detailsDialog = true;
+    },
+  },
+  mounted() {
+    this.getAll();
+    if (localStorage.userId) {
+      // set user_id
+      this.formValues.user_id = localStorage.userId;
+    }
+  },
+};
 </script>
 
 <style scoped>
-  #card {
-    cursor: pointer;
-  }
+#card {
+  cursor: pointer;
+}
 
-  /* striped */
-  tbody tr:nth-of-type(odd) {
-    background-color: rgba(0, 0, 0, 0.05);
-  }
+/* striped */
+tbody tr:nth-of-type(odd) {
+  background-color: rgba(0, 0, 0, 0.05);
+}
 
-  .v-btn:not(.v-btn--round).v-size--default {
-    height: auto;
-  }
+.v-btn:not(.v-btn--round).v-size--default {
+  height: auto;
+}
 
-  .w-100 {
-    width: 100%;
-  }
+.w-100 {
+  width: 100%;
+}
 </style>
